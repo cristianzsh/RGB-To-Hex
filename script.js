de53e2b0158c99b6f8aad6d3bd98cc991f74d84a
cristian-henrique <cristianmsbr@gmail.com>
2016-06-05 08:53:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ function convert() {
 	r = parseInt(document.getElementById("r").value);
 	g = parseInt(document.getElementById("g").value);
 	b = parseInt(document.getElementById("b").value);
-
-	if (r > 255 || g > 255 || b > 255) {
-		document.getElementById("result").innerHTML = "Invalid numbers";
-	} else {
-		res = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-		document.getElementById("result").innerHTML = res.toUpperCase();
-		document.body.style.backgroundColor = res;
-	}
+	
+	res = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	document.getElementById("result").innerHTML = res.toUpperCase();
+	document.body.style.backgroundColor = res;
 	return false;
 }
